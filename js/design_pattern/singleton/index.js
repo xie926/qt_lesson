ad3e3ff60a8d05f09f0627da1c5e509a1266d013
xie926 <1187904163@qq.com>
2019-08-22 10:08:53 +0800

    design_pattern

---
+++ js/design_pattern/singleton/index.js
@@ -0,0 +1,19 @@
+const Singleton = (function() {
+    // 闭包 closure
+    let instance = null; // 闭包区域
+    console.log('------闭包区域')
+    return function() {
+        // constructor
+        console.log('------new')
+        if (instance) {
+            console.log(123)
+            return instance;
+        }
+        return instance = this;
+    }
+})();
+console.log(Singleton, typeof Singleton)
+// 实例 return  单例模式
+const a = new Singleton();
+const b = new Singleton();
+console.log(a == b);
---
+++ js/design_pattern/singleton/readme.md
@@ -0,0 +1,18 @@
+- 单例模式，是设计模式的最基本模式
+不管new多少次，永远返回一个对象
+instance
+- 立即执行函数 + 闭包
+instance 全局？
+var instance = null; // 缺点：污染命名空间
+var Singleton = ()(function () {
+    return function() {
+        if(instance){
+            return instance;
+        }
+        return instance = this; // 创建出来
+    }
+})
+立即执行函数，在返回函数，是构成闭包，常用手段。
+生产环境 立即执行函数  创造闭包环境 变量一直在
+闭包函数在运行时，可以引用到生成时闭包环境中的
+变量
