f38ca461f6766d95c9cc5477589c6be7a91049c5
xie926 <1187904163@qq.com>
2019-11-16 15:34:29 +0800

    vue-router

---
+++ vue/vue-router/src/components/Look.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{msg}}
+    <span>{{$route.params.id}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Look",
+  data() {
+    return {
+      msg: "看一看页面"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
